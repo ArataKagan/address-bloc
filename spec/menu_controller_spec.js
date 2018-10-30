9ea6ff73f089937f01abc3f5bf49771e1d8b07bb
Arata Kagan <aratakagan@gmail.com>
2018-10-30 10:37:43 -0700

    completed testing checkpoint

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -1,15 +1,18 @@
 const MenuController = require("../controllers/MenuController");
 
 describe( "MenuController", () => {
+
+    beforeEach( () => {
+        this.menu = new MenuController();
+    });
+
     describe("getContactCount()", () => {
         it("should return 0 when no contacts are in the book", () => {
-            const menu = new MenuController(); 
-            expect(menu.getContactCount()).toBe(0)
+            expect(this.menu.getContactCount()).toBe(0)
         });
         it("should return 1 when there is exactly one contact in the book", () => {
-            const menu = new MenuController();
-            menu.contacts.push("Bob");
-            expect(menu.getContactCount()).toBe(1)
+            this.menu.contacts.push("Bob");
+            expect(this.menu.getContactCount()).toBe(1)
         });
     });
 });
